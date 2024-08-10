7059e40f3e75ff9a43e1b9e07b0043c4232ebade
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-10 10:21:16 -0700

    Fixed RED code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Duo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Duo.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
-import static android.text.TextUtils.substring;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
@@ -9,15 +7,8 @@ import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.subsystem.Deposit;
 import org.firstinspires.ftc.teamcode.subsystem.risingEdge;
 
@@ -32,6 +23,8 @@ public class Duo extends LinearOpMode {
 
     risingEdge risingEdgeDetector;
 
+    String buttons = "";
+
     @Override
     public void runOpMode() throws InterruptedException {
         Gamepad currentGamepad1 = new Gamepad();
@@ -45,8 +38,8 @@ public class Duo extends LinearOpMode {
 
         while (opModeIsActive()) {
             TelemetryPacket packet = new TelemetryPacket();
-
-            if (gamepad1.left_bumper && checkButton(gamepad1, "left_bumper")) {
+            
+            if (gamepad1.left_bumper && checkButton(currentGamepad1, "left_bumper")) {
                 runningActions.add(new SequentialAction(
                     new InstantAction(deposit::moveWristLeft)
                 ));
@@ -54,7 +47,7 @@ public class Duo extends LinearOpMode {
                 sleep(200);
             }
 
-            else if (gamepad2.right_bumper && checkButton(gamepad2, "right_bumper")) {
+            if (gamepad1.right_bumper && checkButton(currentGamepad1, "right_bumper")) {
                 runningActions.add(new SequentialAction(
                     new InstantAction(deposit::moveWristRight)
                 ));
@@ -77,18 +70,18 @@ public class Duo extends LinearOpMode {
             currentGamepad1.copy(gamepad1);
             currentGamepad2.copy(gamepad2);
 
-            String buttons = String.valueOf(gamepad1).substring(75).substring(1);
-
             telemetry.addData("wristPos", deposit.wrist.getPosition());
-            telemetry.addData("gamepad1", checkButton(gamepad1, "triangle"));
-
-            telemetry.addData("gamepad1", buttons.contains("triangle"));
             telemetry.update();
         }
     }
 
     public boolean checkButton(Gamepad gamepad, String button) {
-        String buttons = String.valueOf(gamepad).substring(75).substring(1);
-        return buttons.contains(button);
+        try {
+            String buttons = String.valueOf(gamepad).substring(75).substring(1);
+            return !buttons.contains(button);
+        }
+        catch (Exception ignored){
+            return (true);
+        }
     }
 }
