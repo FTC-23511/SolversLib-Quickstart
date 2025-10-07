8e547c6c7586b69dff84c20f9a43494fd01b80a5
ClaireO-Brien <claireobrienca@gmail.com>
2025-10-06 17:44:05 -0700

    created IntakeSubsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSubsystem.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    public enum IntakeState {
+        STILL, INTAKING, REVERSE;
+    }
+    public IntakeState intakeState = IntakeState.STILL;
+    private DcMotor intakeWheels;
+    public IntakeSubsystem(final HardwareMap hMap) {
+        intakeWheels = hMap.get(DcMotor.class, "intakeWheels");
+        intakeWheels.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void setSpeed(IntakeState state) {
+        switch(state) {
+            case STILL:
+                intakeWheels.setPower(0.0);
+                break;
+            case INTAKING:
+                intakeWheels.setPower(1.0);
+                break;
+            case REVERSE:
+                intakeWheels.setPower(-1.0);
+                break;
+        }
+    }
+}
