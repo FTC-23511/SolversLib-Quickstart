d1cf53725f4a93fe428accb6c25fcb1c519377eb
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-10-06 17:43:24 -0700

    khbjaergboihjlnrkedfv

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SpindexerSubsystem.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.solversHardware.*;
+
+public class SpindexerSubsystem extends SubsystemBase {
+    private SolversMotor spindexer;
+    public SpindexerSubsystem(final HardwareMap hm) {
+        spindexer =
+    }
+}
