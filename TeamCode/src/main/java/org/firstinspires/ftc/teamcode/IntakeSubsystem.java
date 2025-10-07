0d8123c7be2eaa72cf889af2855f12b2571e35f9
ClaireO-Brien <claireobrienca@gmail.com>
2025-10-06 18:15:49 -0700

    added PID to intake and created RedTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSubsystem.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.controller.PIDFController;
 
 public class IntakeSubsystem extends SubsystemBase {
     public enum IntakeState {
@@ -10,22 +11,35 @@ public class IntakeSubsystem extends SubsystemBase {
     }
     public IntakeState intakeState = IntakeState.STILL;
     private DcMotor intakeWheels;
+    private double kP = 0.000;
+    private double kI = 0.000;
+    private double kD = 0.000;
+    private double kF = 0.000;
+    PIDFController pidf = new PIDFController(kP, kI, kD, kF);
     public IntakeSubsystem(final HardwareMap hMap) {
         intakeWheels = hMap.get(DcMotor.class, "intakeWheels");
         intakeWheels.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    public void setPIDTarget(int num) {
+        pidf.setSetPoint(num);
+    }
+
     public void setSpeed(IntakeState state) {
         switch(state) {
             case STILL:
-                intakeWheels.setPower(0.0);
+                setPIDTarget(0);
                 break;
             case INTAKING:
-                intakeWheels.setPower(1.0);
+                setPIDTarget(1);
                 break;
             case REVERSE:
-                intakeWheels.setPower(-1.0);
+                setPIDTarget(-1);
                 break;
         }
+    }
+
+    public void periodic() {
+        intakeWheels.setPower(pidf.calculate(intakeWheels.getCurrentPosition()));
     }
 }
