463bf695805ebeca3ddad692fc185a31125956c6
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-11-04 18:06:59 -0800

    revert auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedAuto.java
@@ -10,18 +10,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.ParallelRaceGroup;
 import com.seattlesolvers.solverslib.command.RunCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.WaitCommand;
 import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 
+import org.firstinspires.ftc.teamcode.commands.IfRobotStuckThenFinishCommand;
+import org.firstinspires.ftc.teamcode.commands.WaitForColorCommand;
+import org.firstinspires.ftc.teamcode.commands.WaitForShooterCommand;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.ColorSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SpindexerSubsystem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Config
 @Autonomous(name = "Red🦅", group = "angryBirds", preselectTeleOp = "Alpha Teleop")
@@ -55,112 +61,136 @@ public class RedAuto extends CommandOpMode {
     private Follower follower;
 
     //update starting pose
-    public static Pose startingPose = new Pose(108,108,45); //find actual statring pos
+    public static Pose startingPose = new Pose(123.36079077429983,122.17462932454696,Math.toRadians(45)); //find actual statring pos
     private IntakeSubsystem intake;
     private ShooterSubsystem shooter;
     private SpindexerSubsystem spindexer;
+    private ColorSubsystem colorsensor;
     private LEDSubsystem led;
-
 
     public void buildPaths(Follower follower) {
         follower.setStartingPose(startingPose);
+        paths.add(follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(123.361, 122.175), new Pose(84, 84))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(45))
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(108.000, 108.000), new Pose(99.000, 84.000))
+                        new BezierLine(new Pose(84, 84), new Pose(101.000, 84.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(0))
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(99.000, 84.000), new Pose(125.000, 84.000))
+                        new BezierLine(new Pose(101.000, 84.000), new Pose(130.000, 84.000))
                 )
                 .setTangentHeadingInterpolation()
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(125.000, 84.000), new Pose(108.000, 108.000))
+                        new BezierLine(new Pose(130.000, 84.000), new Pose(84, 84))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(45))
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(108.000, 108.000), new Pose(99.000, 60.000))
+                        new BezierLine(new Pose(84,  84), new Pose(95.000, 60.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(0))
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(99.000, 60.000), new Pose(125.000, 60.000))
+                        new BezierLine(new Pose(95.000, 60.000), new Pose(140.000, 60.000))
                 )
                 .setTangentHeadingInterpolation()
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(125.000, 60.000), new Pose(108.000, 108.000))
+                        new BezierLine(new Pose(140.000, 60.000), new Pose(125.000, 60.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                .build()
+        );
+
+        paths.add(follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(125.000, 60.000), new Pose(84, 84))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(45))
-                .build());
+                .build()
+        );
 
         paths.add(follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(108.000, 108.000), new Pose(99.000, 35.000))
+                        new BezierLine(new Pose(84, 84), new Pose(84.000, 108.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(0))
-                .build());
+                .build()
+        );
 
-        paths.add(follower
-                .pathBuilder()
-                .addPath(
-                        new BezierLine(new Pose(99.000, 35.000), new Pose(125.000, 35.000))
-                )
-                .setTangentHeadingInterpolation()
-                .build());
-
-        paths.add(follower
-                .pathBuilder()
-                .addPath(
-                        new BezierLine(new Pose(125.000, 35.000), new Pose(108.000, 108.000))
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(45))
-                .build());
     }
     //preset command methods
     public SequentialCommandGroup shootArtifacts() {
         return new SequentialCommandGroup(
-                new InstantCommand(() -> shooter.setTargetVelocity(1300)),
-                new WaitCommand(1000),
                 new InstantCommand(() -> spindexer.advanceSpindexer()),
-                new WaitCommand(2000),
+                new ParallelRaceGroup(
+                        new WaitForShooterCommand(shooter),
+                        new WaitCommand(1000)
+                ),
                 new InstantCommand(() -> spindexer.advanceSpindexer()),
-                new WaitCommand(2000),
+                new ParallelRaceGroup(
+                        new WaitForShooterCommand(shooter),
+                        new WaitCommand(1000)
+                ),
                 new InstantCommand(() -> spindexer.advanceSpindexer()),
-                new WaitCommand(1000),
-                new InstantCommand(() -> shooter.setTargetVelocity(0))
+                new ParallelRaceGroup(
+                        new WaitForShooterCommand(shooter),
+                        new WaitCommand(1000)
+                )
         );
     }
 
     private SequentialCommandGroup intakeArtifacts() {
         return new SequentialCommandGroup(
                 new InstantCommand(() -> intake.setSpeed(IntakeSubsystem.IntakeState.INTAKING)),
-                new WaitCommand(2000),
+                new ParallelRaceGroup(
+                        new WaitForColorCommand(colorsensor),
+                        new WaitCommand(1500)
+                ),
                 new InstantCommand(() -> spindexer.advanceSpindexer()),
-                new WaitCommand(2000),
+                new ParallelRaceGroup(
+                        new WaitForColorCommand(colorsensor),
+                        new WaitCommand(500)
+                ),
                 new InstantCommand(() -> spindexer.advanceSpindexer()),
-                new WaitCommand(2000),
-                new InstantCommand(() -> intake.setSpeed(IntakeSubsystem.IntakeState.STILL))
+                new ParallelRaceGroup(
+                        new WaitForColorCommand(colorsensor),
+                        new WaitCommand(500)
+                ),
+                new InstantCommand(() -> intake.setSpeed(IntakeSubsystem.IntakeState.REVERSE))
         );
     }
 
@@ -171,9 +201,11 @@ public class RedAuto extends CommandOpMode {
 
         //systems and pedro
         follower = Constants.createFollower(hardwareMap);
+        follower.setMaxPower(1.0);
         intake = new IntakeSubsystem(hardwareMap);
         shooter = new ShooterSubsystem(hardwareMap);
         spindexer = new SpindexerSubsystem(hardwareMap);
+        colorsensor = new ColorSubsystem(hardwareMap);
         led = new LEDSubsystem(hardwareMap);
 
         // DO NOT REMOVE! Resetting FTCLib Command Scheduler
@@ -181,7 +213,7 @@ public class RedAuto extends CommandOpMode {
         super.reset();
 
         // Initialize subsystems
-        register(intake, spindexer, shooter);
+        register(intake, spindexer, shooter, colorsensor, led);
 
         //init paths
         buildPaths(follower);
@@ -194,47 +226,51 @@ public class RedAuto extends CommandOpMode {
         schedule(
                 // DO NOT REMOVE: updates follower to follow path
                 new RunCommand(() -> follower.update()),
-
                 new SequentialCommandGroup(
-                        //starting shoot
+                        new InstantCommand(() -> follower.setMaxPower(1)),
+                        new InstantCommand(() -> {shooter.setTargetVelocity(1200);}), //start shoot
+                        new FollowPathCommand(follower, paths.get(0), true), //drive to shooting pos
+                        new InstantCommand(() -> follower.setMaxPower(1.0)),
+                        new WaitCommand(500),
                         shootArtifacts(),
-
-                        new WaitCommand(1000),
 
                         //cycle one
-                        new FollowPathCommand(follower, paths.get(0)),
-                        new WaitCommand(1000),
+                        new FollowPathCommand(follower, paths.get(1), true), //drives to balls and lines itself up to intake
                         new ParallelCommandGroup(
-                            intakeArtifacts(),
-                            new FollowPathCommand(follower, paths.get(1))
+                                new InstantCommand(() -> follower.setMaxPower(0.3)),
+                                intakeArtifacts(),
+                                new ParallelRaceGroup(
+                                        new FollowPathCommand(follower, paths.get(2), true), //drive and pick up balls
+                                        new IfRobotStuckThenFinishCommand(follower)
+                                )
+
                         ),
-                        new WaitCommand(1000),
-                        new FollowPathCommand(follower, paths.get(2)),
-                        new WaitCommand(1000),
+                        new InstantCommand(() -> follower.setMaxPower(1)),
+                        new FollowPathCommand(follower, paths.get(3), true), // returning to shooting pos
+                        //needs time for shooter to ramp up
                         shootArtifacts(),
 
-                        new WaitCommand(1000),
-
                         //cycle two
-                        new FollowPathCommand(follower, paths.get(3)),
-                        new WaitCommand(1000),
+                        new FollowPathCommand(follower, paths.get(4), true), //drives to balls and lines itself up to intake
                         new ParallelCommandGroup(
+                                new InstantCommand(() -> follower.setMaxPower(0.3)),
                                 intakeArtifacts(),
-                                new FollowPathCommand(follower, paths.get(4))
+                                new ParallelRaceGroup(
+                                        new FollowPathCommand(follower, paths.get(5), true), //drive and pick up balls
+                                        new IfRobotStuckThenFinishCommand(follower)
+                                )
                         ),
-                        new WaitCommand(1000),
-                        new FollowPathCommand(follower, paths.get(5)),
-                        new WaitCommand(1000),
-                        shootArtifacts()
-                        /*
-                        //cycle three
-                        new FollowPathCommand(follower, paths.get(6)),
-                        new ParallelCommandGroup(
-                                intakeArtifacts(),
-                                new FollowPathCommand(follower, paths.get(7))
-                        ),
-                        new FollowPathCommand(follower, paths.get(8)),
-                        shootArtifacts()*/
+                        //needs extra step to back out from the wall because it will collide with the exit of the ramp
+                        new InstantCommand(() -> follower.setMaxPower(1)),
+                        new FollowPathCommand(follower, paths.get(6), true),
+
+                        new FollowPathCommand(follower, paths.get(7), true), //return to shooting pos
+                        shootArtifacts(),
+
+                        //move off shooting line so that you get extra points theoretically
+                        new FollowPathCommand(follower, paths.get(8), true),
+
+                        new InstantCommand(() -> {shooter.setTargetVelocity(0);})
                 )
         );
 
@@ -252,6 +288,12 @@ public class RedAuto extends CommandOpMode {
             led.setColor(LEDSubsystem.LEDState.GREEN);
         }
 
+/*        telemetry.addData("current pos", String.format("X: %8.2f, Y: %8.2f", follower.getPose().getX(), follower.getPose().getY()));
+        telemetry.addData("current heading", String.format("Heading: %.4f", follower.getPose().getHeading()));*/
+
+        telemetry.addData("current pos", follower.getPose().getX() + " " + follower.getPose().getY());
+        telemetry.addData("current heading", Math.toDegrees(follower.getPose().getHeading()));
+
         telemetry.addData("spindexer output", spindexer.getOutput());
         telemetry.addData("spindexer setpoint", spindexer.getPIDSetpoint());
         telemetry.addData("spindexer pos", spindexer.getCurrentPosition());
@@ -260,6 +302,9 @@ public class RedAuto extends CommandOpMode {
 
         telemetry.addData("shooter target velocity", shooter.getTargetVelocity());
         telemetry.addData("shooter actual velocity", shooter.getActualVelocity());
+        telemetry.addData("green color detected?", Arrays.toString(colorsensor.senseColor()));
+        telemetry.addData("green color detected?", colorsensor.checkIfGreen());
+        telemetry.addData("purple color detected?", colorsensor.checkIfPurple());
         telemetry.update();
         super.run();
     }
