ffe3550a7ae2dd6a4ecf14c4b6bff61157a9ad4f
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-11-12 18:17:42 -0800

    refactor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GatePositionTuningOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GatePositionTuningOp.java
@@ -1,46 +1,41 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
-import com.seattlesolvers.solverslib.hardware.ServoEx;
 
-import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.GateSubsystem;
 
-@TeleOp(name = "Pivot Degrees")
-public class pivotDegrees extends CommandOpMode{
+@Config
+@TeleOp(name = "Gate Position Tuning")
+public class GatePositionTuningOp extends CommandOpMode {
     public GamepadEx driver2;
-    private ShooterSubsystem shooter;
-
+    private GateSubsystem gate;
     @Override
     public void initialize() {
         driver2 = new GamepadEx(gamepad2);
-        shooter = new ShooterSubsystem(hardwareMap);
-        //when starting, it will try its hardest to get to position of 0 ticks
-        //check telemetry to see pivot actual position and use buttons to adjust to 0 degrees flat
-        //set that position as the offset so every time subsystem initializes pivot to that position
+        gate = new GateSubsystem(hardwareMap);
 
         driver2.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(
                 new InstantCommand(() -> {
-                    shooter.increasePivotPosition(0.001);
+                    gate.gateUp();
                 })
         );
         driver2.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(
                 new InstantCommand(() -> {
-                    shooter.decreasePivotPosition(0.001);
+                    gate.gateDown();
                 })
         );
     }
 
     @Override
     public void run() {
-        double pivotPosition = shooter.getPivotPosition();
+        double gatePosition = gate.getGatePosition();
 
-        telemetry.addData("pivot current position: ", pivotPosition);
+        telemetry.addData("gate position: ", gatePosition);
         telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PivotDegreesOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PivotDegreesOp.java
@@ -1,44 +1,43 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
-import org.firstinspires.ftc.teamcode.subsystems.GateSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
 
-@Config
-@TeleOp(name = "Gate Position Tuning")
-public class gatePositionTuning extends CommandOpMode {
+@TeleOp(name = "Pivot Degrees")
+public class PivotDegreesOp extends CommandOpMode{
     public GamepadEx driver2;
-    private GateSubsystem gate;
+    private ShooterSubsystem shooter;
+
     @Override
     public void initialize() {
         driver2 = new GamepadEx(gamepad2);
-        gate = new GateSubsystem(hardwareMap);
+        shooter = new ShooterSubsystem(hardwareMap);
+        //when starting, it will try its hardest to get to position of 0 ticks
+        //check telemetry to see pivot actual position and use buttons to adjust to 0 degrees flat
+        //set that position as the offset so every time subsystem initializes pivot to that position
 
         driver2.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(
                 new InstantCommand(() -> {
-                    gate.gateUp();
+                    shooter.increasePivotPosition(0.001);
                 })
         );
         driver2.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(
                 new InstantCommand(() -> {
-                    gate.gateDown();
+                    shooter.decreasePivotPosition(0.001);
                 })
         );
     }
 
     @Override
     public void run() {
-        double gatePosition = gate.getGatePosition();
+        double pivotPosition = shooter.getPivotPosition();
 
-        telemetry.addData("gate position: ", gatePosition);
+        telemetry.addData("pivot current position: ", pivotPosition);
         telemetry.update();
     }
 }
