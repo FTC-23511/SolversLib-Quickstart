edbb8ce07521337f166e473bf8b0b3ab1eb20d78
ClaireO-Brien <claireobrienca@gmail.com>
2025-11-18 17:03:25 -0800

    added goal distance apriltag detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraSubsystem.java
@@ -9,7 +9,6 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainCon
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import org.opencv.core.Point;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +16,8 @@ import java.util.concurrent.TimeUnit;
 
 public class CameraSubsystem extends SubsystemBase {
     private static final boolean USE_WEBCAM = true;
-    private static final List<Integer> DESIRED_TAG_ID = Arrays.asList(21, 22, 23); // Tags we should detect
+    private static final List<Integer> MOTIF_TAG_IDS = Arrays.asList(21, 22, 23); // Tags we should detect for motif
+    private static final List<Integer> DISTANCE_TAG_IDS = Arrays.asList(20, 24); // Tags we should detect for goal distance
     private VisionPortal visionPortal; // Used to manage the video source.
     private AprilTagProcessor myAprilTagProcessor; // Used for managing the AprilTag detection process.
     //private AprilTagDetection myAprilTagDetection = null; // Used to hold the data for a detected AprilTag
@@ -85,7 +85,7 @@ public class CameraSubsystem extends SubsystemBase {
         // Step through the list of detected tags and look for a matching tag
         for (AprilTagDetection detection : myAprilTagDetections) {
             // Look to see if we have size info on this tag.
-            if (DESIRED_TAG_ID.contains(detection.id)) {
+            if (MOTIF_TAG_IDS.contains(detection.id)) {
                 area = 0.5*Math.abs(
                         (detection.corners[0].x * detection.corners[1].y)
                         + (detection.corners[1].x * detection.corners[2].y)
@@ -95,6 +95,49 @@ public class CameraSubsystem extends SubsystemBase {
                         - (detection.corners[2].x * detection.corners[1].y)
                         - (detection.corners[3].x * detection.corners[2].y)
                         - (detection.corners[0].x * detection.corners[3].y)
+                );
+                if (area > max_area) {
+                    max_area = area;
+                    max = detection;
+                }
+            }
+        }
+        // check to see if detected tag id matches the available motifs
+        return max.id;
+    }
+
+    public int detectGoalDistance() {
+        List<AprilTagDetection> myAprilTagDetections;
+        double area;
+
+        myAprilTagDetections = myAprilTagProcessor.getDetections();
+        if (myAprilTagDetections.isEmpty()) {
+            return -1;
+        }
+        AprilTagDetection max = myAprilTagDetections.get(0);
+        double max_area = 0.5*Math.abs(
+                (max.corners[0].x * max.corners[1].y)
+                        + (max.corners[1].x * max.corners[2].y)
+                        + (max.corners[2].x * max.corners[3].y)
+                        + (max.corners[3].x * max.corners[0].y)
+                        - (max.corners[1].x * max.corners[0].y)
+                        - (max.corners[2].x * max.corners[1].y)
+                        - (max.corners[3].x * max.corners[2].y)
+                        - (max.corners[0].x * max.corners[3].y)
+        );
+        // Step through the list of detected tags and look for a matching tag
+        for (AprilTagDetection detection : myAprilTagDetections) {
+            // Look to see if we have size info on this tag.
+            if (DISTANCE_TAG_IDS.contains(detection.id)) {
+                area = 0.5*Math.abs(
+                        (detection.corners[0].x * detection.corners[1].y)
+                                + (detection.corners[1].x * detection.corners[2].y)
+                                + (detection.corners[2].x * detection.corners[3].y)
+                                + (detection.corners[3].x * detection.corners[0].y)
+                                - (detection.corners[1].x * detection.corners[0].y)
+                                - (detection.corners[2].x * detection.corners[1].y)
+                                - (detection.corners[3].x * detection.corners[2].y)
+                                - (detection.corners[0].x * detection.corners[3].y)
                 );
                 if (area > max_area) {
                     max_area = area;
