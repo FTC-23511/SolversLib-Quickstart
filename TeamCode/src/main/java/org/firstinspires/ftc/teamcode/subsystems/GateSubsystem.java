8dcecda7aa128064ca0466af3e7cd5ccecb8d123
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2026-01-08 18:35:13 -0800

    Update GateSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GateSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GateSubsystem.java
@@ -1,29 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.hardware.ServoEx;
 
 public class GateSubsystem extends SubsystemBase {
-//    ServoEx gateLeft;
-    ServoEx gateRight;
-    public double UP = 0.55;
-    public double DOWN = 0.40;
+    ServoEx gate;
+    AnalogInput gateEncoder;
+    public final double UP = 0.55;
+    public final double DOWN = 0.40;
+    public final double UP_VOLTAGE = 1.6; //Volts
+    public final double DOWN_VOLTAGE = 1.3; //Volts
+
     public GateSubsystem(final HardwareMap hMap) {
-//        gateLeft = new ServoEx(hMap, "gateLeft");
-        gateRight = new ServoEx(hMap, "gateRight");
-//        gateLeft.setInverted(false);
-        gateRight.setInverted(true);
+        gate = new ServoEx(hMap, "gate");
+        gate.setInverted(true);
     }
     public void up() {
-//        gateLeft.set(UP);
-        gateRight.set(UP);
+        gate.set(UP);
     }
     public void down() {
-//        gateLeft.set(DOWN);
-        gateRight.set(DOWN);
+        gate.set(DOWN);
     }
-    public double getPosition() {
-        return gateRight.getRawPosition();
+    public double getEncoderVoltage() {
+        return gateEncoder.getVoltage();
     }
+    public boolean isAtTarget(){
+        //86fuyuiyftuvjh
+    }
+
 }
