db62c7d70e989ecb088c4b6cbc488fa384703993
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2026-01-09 16:42:48 -0800

    Add side wheel control to IntakeSubsystem
    Introduces two CRServoEx side wheels to the IntakeSubsystem and updates the set() method to control their power based on the intake state. This enhances the intake mechanism by allowing coordinated control of additional side wheels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.motors.CRServoEx;
 
 public class IntakeSubsystem extends SubsystemBase {
     public enum IntakeState {
@@ -10,21 +12,31 @@ public class IntakeSubsystem extends SubsystemBase {
     }
     public IntakeState intakeState = IntakeState.STILL;
     private DcMotor intakeWheels;
+    private CRServoEx sideWheel;
+    private CRServoEx sideWheel2;
     public IntakeSubsystem(final HardwareMap hMap) {
         intakeWheels = hMap.get(DcMotor.class, "intake");
         intakeWheels.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        sideWheel = hMap.get(CRServoEx.class, "intakeSide1");
+        sideWheel2 = hMap.get(CRServoEx.class, "intakeSide2");
     }
 
     public void set(IntakeState state) {
         switch(state) {
             case STILL:
                 intakeWheels.setPower(0.0);
+                sideWheel.set(0.0);
+                sideWheel2.set(0.0);
                 break;
             case INTAKING:
                 intakeWheels.setPower(1.0);
+                sideWheel.set(1.0);
+                sideWheel2.set(-1.0);
                 break;
             case REVERSE:
                 intakeWheels.setPower(-1.0);
+                sideWheel.set(-1.0);
+                sideWheel2.set(1.0);
                 break;
         }
         intakeState = state;
